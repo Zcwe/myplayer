0dba3995646eb407c2d207ef220b19b8a90f0ce4
Zcwe <z3607943561c@foxmail.com>
2026-01-11 17:31:27 +0800

    打包成功

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// src/reportWebVitals.js 完整原生代码 (CRA默认，直接复制)
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
